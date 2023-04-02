d80d6c90b4a92b195a573c472060bfb0b81ae8cc
d <d@ds-Mac-mini.local>
2023-04-02 16:04:13 -0700

    update hardhat config

--- my-metamask-image-classifier/hardhat.config.js
+++ my-metamask-image-classifier/hardhat.config.js
@@ -2,5 +2,11 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.0",
+	networks: {
+       rinkeby: {
+         url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+         accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+       },
+	}
 };
