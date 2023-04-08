9128fdb0dc7269c6e16d25d121ae023279732bb5
Duncan Wilson <duncanw@nevada.unr.edu>
2023-04-08 11:48:13 -0700

    update app  to gate rss lmao

--- my-metamask-image-classifier/src/App.js
+++ my-metamask-image-classifier/src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import feedparser from "feedparser";
 
 function App() {
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+  const [connectedWallet, setConnectedWallet] = useState("");
+
+  // Connect to Metamask wallet
+  const connectMetamask = async () => {
+    if (window.ethereum) {
+      try {
+        const web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+        const accounts = await web3.eth.getAccounts();
+        setWeb3(web3);
+        setAccounts(accounts);
+        setConnectedWallet("Metamask");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  // Connect to WalletConnect wallet
+  const connectWalletConnect = async () => {
+    const provider = new WalletConnectProvider({
+      rpc: {
+        1: "https://mainnet.infura.io/v3/your-infura-id",
+        3: "https://ropsten.infura.io/v3/your-infura-id",
+      },
+    });
+    await provider.enable();
+    const web3 = new Web3(provider);
+    const accounts = await web3.eth.getAccounts();
+    setWeb3(web3);
+    setAccounts(accounts);
+    setConnectedWallet("WalletConnect");
+  };
+
+  // Fetch RSS feed
+  const fetchFeed = async () => {
+    const rss_url = "https://example.com/feed";
+    const response = await fetch(rss_url);
+    const xml = await response.text();
+    const feed = await parseXml(xml);
+    console.log(feed);
+  };
+
+  // Parse XML using feedparser library
+  const parseXml = async (xml) => {
+    const result = await new Promise((resolve, reject) => {
+      const feed = new feedparser();
+      const items = [];
+
+      feed.on("readable", function () {
+        let item;
+        while ((item = feed.read())) {
+          items.push(item);
+        }
+      });
+
+      feed.on("end", function () {
+        resolve(items);
+      });
+
+      feed.on("error", function (error) {
+        reject(error);
+      });
+
+      feed.write(xml);
+      feed.end();
+    });
+
+    return result;
+  };
+
+  useEffect(() => {
+    if (web3) {
+      fetchFeed();
+    }
+  }, [web3]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {connectedWallet ? (
+        <p>Connected to {connectedWallet}</p>
+      ) : (
+        <div>
+          <button onClick={connectMetamask}>Connect to Metamask</button>
+          <button onClick={connectWalletConnect}>Connect to WalletConnect</button>
+        </div>
+      )}
     </div>
   );
 }
